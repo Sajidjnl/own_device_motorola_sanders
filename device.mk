60b54e5691b03f9c0b766a700f46e0c013a4822f
rajadeja <raj.jdja@gmail.com>
2018-04-06 18:16:55 +0000

    build Camera2 instead of Snap

--- device.mk
+++ device.mk
@@ -103,7 +103,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libbson \
     libshim_camera \
-    Snap \
+    Camera2 \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
